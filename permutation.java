178e6d12cd4140b466377a73697519ea675fa87d
VaideshWaranR <vaiv64259@gmail.com>
2023-11-17 07:59:35 +0530

    new

--- permutation.java
+++ permutation.java
@@ -1,37 +1,28 @@
-import java.util.ArrayList;
-
-/**
- * permutation
- */
-public class permutation {
-static ArrayList<Integer[]> res=new ArrayList<Integer[]>();
-    public static void main(String[] args) {
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> ans=new ArrayList<List<Integer>>();
+        per(nums,ans,0);
+        return ans;
         
-        int nums[]=new int[3];
-        nums[0]=1;
-        nums[1]=2;
-        nums[2]=3;
-        permute(nums,0);
     }
-    public static void permute(int []arr,int fi){
-        if(fi==arr.length-1){
-          for(int num:arr)
-          System.out.print(num+" ");
-          System.out.println();
-          return;
+    public void per(int[] arr,List<List<Integer>> ans,int fi){
+        if(fi==arr.length){
+            List<Integer> p=new ArrayList<>();
+            for(int num:arr)
+                p.add(num);
+            ans.add(p);
         }
         else{
             for(int i=fi;i<arr.length;i++){
-           swap(arr,i,fi);
-           permute(arr,fi+1);
-           swap(arr,i,fi);
-            }
+            swap(arr,i,fi);
+                per(arr,ans,fi+1);
+                swap(arr,i,fi);
         }
     }
-    public static void swap(int []arr,int a,int b){
-    int temp=arr[a];
-    arr[a]=arr[b];
-    arr[b]=temp;
-}
-
+    }
+    public void swap(int[] arr,int i,int j){
+        int temp=arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
+    }
 }
